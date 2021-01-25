b3f70a4613a8a925c557198341ab284d031d20c6
Anu Virtanen <virtanenanum@gmail.com>
2021-01-25 10:29:12 +0200

    add responsivity and allReady boolean with ngIf

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,66 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import {ProductService} from '../product.service';
-import {faCheck} from '@fortawesome/free-solid-svg-icons';
-import {faMinus} from '@fortawesome/free-solid-svg-icons';
-
+import { ProductService } from '../product.service';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faMinus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.css'],
 })
 export class ItemComponent implements OnInit {
-phoneId: string = '';
-productsList: any[];
-chosenPhone: any = "";
-faCheck = faCheck;
-faMinus = faMinus;
-newImgSrc: string = '';
+  phoneId: string = '';
+  productsList: any[];
+  chosenPhone: any = '';
+  faCheck = faCheck;
+  faMinus = faMinus;
+  newImgSrc: string = '';
+  allReady: boolean = false;
 
-
-  constructor(  private route: ActivatedRoute,
+  constructor(
+    private route: ActivatedRoute,
     private productService: ProductService,
-    private location: Location) { }
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
     this.getPhone();
-
-
   }
 
   getPhone(): void {
     this.phoneId = this.route.snapshot.params['id'];
 
-    this.route.params.subscribe(
-      (params) => {
-
-        this.phoneId = params['id'];
-      }
-    );
-this.productService.getProduct(this.phoneId).subscribe(
-  data => {this.chosenPhone = data;}
-);
+    this.route.params.subscribe((params) => {
+      this.phoneId = params['id'];
+    });
+    this.productService.getProduct(this.phoneId).subscribe((data) => {
+      this.chosenPhone = data;
+      this.allReady = true;
+    });
 
 
   }
-
-
-
 
   goBack(): void {
     this.location.back();
   }
 
-
-
   changeImg(event: any) {
- this.newImgSrc = event.target.getAttribute('src');
-document.getElementById("view-img").setAttribute('src', this.newImgSrc);
-
+    this.newImgSrc = event.target.getAttribute('src');
+    document.getElementById('view-img').setAttribute('src', this.newImgSrc);
   }
-
-
-
 }
--- src/app/phones-list/phones-list.component.ts
+++ src/app/phones-list/phones-list.component.ts
@@ -1,56 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 
-
 @Component({
   selector: 'app-phones-list',
   templateUrl: './phones-list.component.html',
-  styleUrls: ['./phones-list.component.css']
+  styleUrls: ['./phones-list.component.css'],
 })
 export class PhonesListComponent implements OnInit {
-
   title = 'Phones';
 
-soughtPhone: string = '';
-term: string = '';
-
+  soughtPhone: string = '';
+  term: string = '';
 
   productsList: any[];
   selectedSort: string = '';
 
-
-  constructor(public productService: ProductService) { }
+  constructor(public productService: ProductService) {}
 
   ngOnInit(): void {
     this.getProducts();
   }
 
   getProducts(): void {
-    this.productService.getAllProducts()
-        .subscribe(data => { this.productsList = data;
-          },
-          error => {console.log("http-error:");
-          console.log(error);});
+    this.productService.getAllProducts().subscribe(
+      (data) => {
+        this.productsList = data;
+      },
+      (error) => {
+        console.log('http-error:');
+        console.log(error);
+      }
+    );
   }
 
   sortBy(event: any) {
     this.selectedSort = event.target.value;
-    if (this.selectedSort =="name") {
-    this.productsList.sort(function(a, b) {
-      if (a.name>b.name)
-      return 1;
-      else if (a.name<b.name)
-      return -1;
-      return 0;
-  });}
-    else if (this.selectedSort=="age") {
-      this.productsList.sort(function(a, b) {
+    if (this.selectedSort == 'name') {
+      this.productsList.sort(function (a, b) {
+        if (a.name > b.name) return 1;
+        else if (a.name < b.name) return -1;
+        return 0;
+      });
+    } else if (this.selectedSort == 'age') {
+      this.productsList.sort(function (a, b) {
         return parseInt(b.age) - parseInt(a.age);
-    });
+      });
     }
   }
 
-/*  onSearch(event: any) {
+  /*  onSearch(event: any) {
 
     this.filter = (<HTMLInputElement>event.target).value;
     this.productsList.forEach(element => {
@@ -67,15 +65,4 @@ term: string = '';
 
 
       }*/
-
- }
-
-
-
-
-
-
-
-
-
-
+}
